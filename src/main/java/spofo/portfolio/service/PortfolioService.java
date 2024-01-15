778902a8e7bad4e8fc4f8c92dacea4401a5ad5cd
JANG-WON-JUN <jangwonjun0509@gmail.com>
2024-01-15 14:46:28 +0900

    Remove: PortfolioService 인터페이스 삭제

--- src/main/java/spofo/portfolio/service/PortfolioService.java
+++ src/main/java/spofo/portfolio/service/PortfolioService.java
@@ -10,7 +10,6 @@ import org.springframework.transaction.annotation.Transactional;
 import spofo.global.domain.exception.PortfolioNotFound;
 import spofo.holdingstock.controller.port.HoldingStockService;
 import spofo.holdingstock.domain.HoldingStock;
-import spofo.portfolio.controller.port.PortfolioService;
 import spofo.portfolio.controller.request.PortfolioSearchCondition;
 import spofo.portfolio.domain.Portfolio;
 import spofo.portfolio.domain.PortfolioCreate;
@@ -24,13 +23,12 @@ import spofo.stock.service.StockServerService;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class PortfolioServiceImpl implements PortfolioService {
+public class PortfolioService {
 
     private final PortfolioRepository portfolioRepository;
     private final HoldingStockService holdingStockService;
     private final StockServerService stockServerService;
 
-    @Override
     public TotalPortfoliosStatistic getPortfoliosStatistic(Long memberId,
             PortfolioSearchCondition condition) {
         List<Portfolio> portfolios = portfolioRepository.findByMemberIdWithTradeLogs(memberId);
@@ -39,7 +37,6 @@ public class PortfolioServiceImpl implements PortfolioService {
         return TotalPortfoliosStatistic.of(portfolioStatistics);
     }
 
-    @Override
     public List<PortfolioStatistic> getPortfolios(Long memberId,
             PortfolioSearchCondition condition) {
         List<Portfolio> portfolios =
@@ -48,25 +45,21 @@ public class PortfolioServiceImpl implements PortfolioService {
         return getPortfolioStatistics(portfolios);
     }
 
-    @Override
     public Portfolio getPortfolio(Long id) {
         return findById(id);
     }
 
-    @Override
     public PortfolioStatistic getPortfolioStatistic(Long id) {
         Portfolio portfolio = getPortfolioFrom(portfolioRepository.findByIdWithTradeLogs(id));
         return getPortfolioStatistics(List.of(portfolio)).get(0);
     }
 
-    @Override
     @Transactional
     public Portfolio create(PortfolioCreate request, Long memberId) {
         Portfolio portfolio = Portfolio.of(request, memberId);
         return portfolioRepository.save(portfolio);
     }
 
-    @Override
     @Transactional
     public Portfolio update(PortfolioUpdate request, Long id, Long memberId) {
         Portfolio savedPortfolio = findById(id);
@@ -74,7 +67,6 @@ public class PortfolioServiceImpl implements PortfolioService {
         return portfolioRepository.save(updatedPortfolio);
     }
 
-    @Override
     @Transactional
     public void delete(Long id) {
         Portfolio portfolio = findById(id);
