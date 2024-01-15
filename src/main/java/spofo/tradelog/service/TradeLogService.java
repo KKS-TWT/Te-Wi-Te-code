74e6ce93c774d4faaa4d8dd1d5e2f3edaac6a847
JANG-WON-JUN <jangwonjun0509@gmail.com>
2024-01-15 14:53:44 +0900

    Remove: TradeLogService 인터페이스 삭제

--- src/main/java/spofo/tradelog/service/TradeLogService.java
+++ src/main/java/spofo/tradelog/service/TradeLogService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import spofo.holdingstock.domain.HoldingStock;
 import spofo.stock.domain.Stock;
 import spofo.stock.service.StockServerService;
-import spofo.tradelog.controller.port.TradeLogService;
 import spofo.tradelog.domain.TradeLog;
 import spofo.tradelog.domain.TradeLogCreate;
 import spofo.tradelog.domain.TradeLogStatistic;
@@ -14,19 +13,17 @@ import spofo.tradelog.service.port.TradeLogRepository;
 
 @Service
 @RequiredArgsConstructor
-public class TradeLogServiceImpl implements TradeLogService {
+public class TradeLogService {
 
     private final TradeLogRepository tradeLogRepository;
     private final StockServerService stockServerService;
 
-    @Override
     public TradeLog create(TradeLogCreate request, HoldingStock holdingStock) {
         Stock stock = stockServerService.getStock(holdingStock.getStockCode());
         TradeLog tradeLog = TradeLog.of(request, holdingStock, stock);
         return tradeLogRepository.save(tradeLog);
     }
 
-    @Override
     public List<TradeLogStatistic> getStatistics(Long stockId) {
         List<TradeLog> tradeLogs = tradeLogRepository.findByHoldingStockEntityId(stockId);
         return tradeLogs.stream()
@@ -34,12 +31,10 @@ public class TradeLogServiceImpl implements TradeLogService {
                 .toList();
     }
 
-    @Override
     public void deleteByHoldingStockId(Long id) {
         tradeLogRepository.deleteByHoldingStockId(id);
     }
 
-    @Override
     public void deleteByHoldingStockIds(List<Long> ids) {
         tradeLogRepository.deleteByHoldingStockEntityIdIn(ids);
     }
